1b8cf082d249378320b7ad1daa705f416f69af69
junhao <gongjunhao@sina.com>
2017-12-14 10:47:33 +0800

    checksoft

--- core/src/main/java/com/gjh6/car/utils/HttpClientUtils.java
+++ core/src/main/java/com/gjh6/car/utils/HttpClientUtils.java
@@ -1,40 +1,53 @@
 package com.gjh6.car.utils;
 
 import com.alibaba.fastjson.JSONObject;
+import com.gjh6.car.model.Android;
+import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLContext;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+
 public class HttpClientUtils {
+    private static final Logger logger = LoggerFactory.getLogger(HttpClientUtils.class);
+    private static int SocketTimeout = 10000;//10秒
+    private static int ConnectTimeout = 10000;//10秒
+    private static Boolean SetTimeOut = true;
 
     //使用volatile关键字保其可见性
     volatile private static HttpClientUtils instance = null;
@@ -60,138 +73,170 @@ public class HttpClientUtils {
         return instance;
     }
 
-
-
     /**
-     * 设置信任自签名证书
-     *
-     * @param keyStorePath 密钥库路径
-     * @param keyStorepass 密钥库密码
+     * 构建客户端
+     * 支持https
      * @return
      */
-    public SSLContext custom(String keyStorePath, String keyStorepass) {
-        SSLContext sc = null;
-        FileInputStream instream = null;
-        KeyStore trustStore = null;
+    private CloseableHttpClient getHttpClient() {
+        RegistryBuilder<ConnectionSocketFactory> registryBuilder = RegistryBuilder.<ConnectionSocketFactory>create();
+        ConnectionSocketFactory plainSF = new PlainConnectionSocketFactory();
+        registryBuilder.register("http", plainSF);
+        //指定信任密钥存储对象和连接套接字工厂
         try {
-            trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            instream = new FileInputStream(new File(keyStorePath));
-            trustStore.load(instream, keyStorepass.toCharArray());
-            // 相信自己的CA和所有自签名的证书
-            sc = SSLContexts.custom().loadTrustMaterial(trustStore, new TrustSelfSignedStrategy()).build();
-        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException | KeyManagementException e) {
-            e.printStackTrace();
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            //信任任何链接
+            TrustStrategy anyTrustStrategy = new TrustStrategy() {
+                @Override
+                public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                    return true;
+                }
+            };
+            SSLContext sslContext = SSLContexts.custom().useTLS().loadTrustMaterial(trustStore, anyTrustStrategy).build();
+            LayeredConnectionSocketFactory sslSF = new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            registryBuilder.register("https", sslSF);
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        } catch (KeyManagementException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        Registry<ConnectionSocketFactory> registry = registryBuilder.build();
+        //设置连接管理器
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(registry);
+        //构建客户端
+        return HttpClientBuilder.create().setConnectionManager(connManager).build();
+    }
+
+    /**
+     * get
+     *
+     * @param url     请求的url
+     * @param queries 请求的参数，在浏览器？后面的数据，没有可以传null
+     * @return
+     * @throws IOException
+     */
+    public String get(String url, Map<String, String> queries) throws IOException {
+        String responseBody = "";
+        //支持https
+        CloseableHttpClient httpClient = getHttpClient();
+
+        StringBuilder sb = new StringBuilder(url);
+
+        appendQueryParams(queries, sb);
+
+        HttpGet httpGet = new HttpGet(sb.toString());
+        if (SetTimeOut) {
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setSocketTimeout(SocketTimeout)
+                    .setConnectTimeout(ConnectTimeout).build();//设置请求和传输超时时间
+            httpGet.setConfig(requestConfig);
+        }
+        try {
+            logger.info("Executing request " + httpGet.getRequestLine());
+            //请求数据
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+            logger.info(String.valueOf(response.getStatusLine()));
+            int status = response.getStatusLine().getStatusCode();
+            if (status == HttpStatus.SC_OK) {
+                HttpEntity entity = response.getEntity();
+                responseBody = EntityUtils.toString(entity);
+            } else {
+                logger.info("http return status error:" + status);
+                throw new ClientProtocolException("Unexpected response status: " + status);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
         } finally {
-            try {
-                instream.close();
-            } catch (IOException e) {
-            }
+            httpClient.close();
         }
-        return sc;
+        return responseBody;
     }
 
-
     /**
-     * 模拟请求
-     *
-     * @param url      资源地址
-     * @param map      参数列表
-     * @param encoding 编码
+     * url追加参数
+     * @param queries
+     * @param sb
+     */
+    private void appendQueryParams(Map<String, String> queries, StringBuilder sb) {
+        if (queries != null && queries.keySet().size() > 0) {
+            boolean firstFlag = true;
+            Iterator iterator = queries.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry<String, String>) iterator.next();
+                if (firstFlag) {
+                    sb.append("?" + (String) entry.getKey() + "=" + (String) entry.getValue());
+                    firstFlag = false;
+                } else {
+                    sb.append("&" + (String) entry.getKey() + "=" + (String) entry.getValue());
+                }
+            }
+        }
+    }
+
+    /** post
+     * @param url     请求的url
+     * @param queries 请求的参数，在浏览器？后面的数据，没有可以传null
+     * @param params  post form 提交的参数
      * @return
      * @throws IOException
-     * @throws KeyManagementException
-     * @throws NoSuchAlgorithmException
-     * @throws ClientProtocolException
      */
-    public String send(String url, Map<String, String> map, String encoding) throws ClientProtocolException, IOException {
-        String body = "";
+    public String post(String url, Map<String, String> queries, Map<String, String> params) throws IOException {
+        String responseBody = "";
 
-        //tomcat是我自己的密钥库的密码，你可以替换成自己的
-        //如果密码为空，则用"nopassword"代替
-        SSLContext sslcontext = custom("D:\\keys\\wsriakey", "tomcat");
+        CloseableHttpClient httpClient = getHttpClient();
 
-        // 设置协议http和https对应的处理socket链接工厂的对象
-        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
-                .register("http", PlainConnectionSocketFactory.INSTANCE)
-                .register("https", new SSLConnectionSocketFactory(sslcontext))
-                .build();
-        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-        HttpClients.custom().setConnectionManager(connManager);
+        StringBuilder sb = new StringBuilder(url);
 
-        //创建自定义的httpclient对象
-        CloseableHttpClient client = HttpClients.custom().setConnectionManager(connManager).build();
-//		CloseableHttpClient client = HttpClients.createDefault();
+        appendQueryParams(queries, sb);
 
-        //创建post方式请求对象
-        HttpPost httpPost = new HttpPost(url);
-
-        //装填参数
+        //指定url,和http方式
+        HttpPost httpPost = new HttpPost(sb.toString());
+        if (SetTimeOut) {
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setSocketTimeout(SocketTimeout)
+                    .setConnectTimeout(ConnectTimeout).build();//设置请求和传输超时时间
+            httpPost.setConfig(requestConfig);
+        }
+        //添加参数
         List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-        if (map != null) {
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                nvps.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+        if (params != null && params.keySet().size() > 0) {
+            Iterator<Map.Entry<String, String>> iterator = params.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, String> entry = (Map.Entry<String, String>) iterator.next();
+                nvps.add(new BasicNameValuePair((String) entry.getKey(), (String) entry.getValue()));
             }
         }
-        //设置参数到请求对象中
-        httpPost.setEntity(new UrlEncodedFormEntity(nvps, encoding));
-
-        System.out.println("请求地址：" + url);
-        System.out.println("请求参数：" + nvps.toString());
-
-        //设置header信息
-        //指定报文头【Content-type】、【User-Agent】
-        httpPost.setHeader("Content-type", "application/x-www-form-urlencoded");
-        //httpPost.setHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
-
-        //执行请求操作，并拿到结果（同步阻塞）
-        CloseableHttpResponse response = client.execute(httpPost);
-        //获取结果实体
-        HttpEntity entity = response.getEntity();
-        if (entity != null) {
-            //按指定编码转换结果实体为String类型
-            body = EntityUtils.toString(entity, encoding);
-        }
-        EntityUtils.consume(entity);
-        //释放链接
-        response.close();
-        return body;
+        httpPost.setEntity(new UrlEncodedFormEntity(nvps, Consts.UTF_8));
+        //请求数据
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        responseBody = getResponseString(responseBody, response);
+        return responseBody;
     }
 
 
-    /**
-     * 模拟请求
-     *
-     * @param url      资源地址
-     * @param obj      参数对象转JSON
-     * @param encoding 编码
+    /** post
+     * @param url     请求的url
+     * @param queries 请求的参数，在浏览器？后面的数据，没有可以传null
+     * @param obj       post obj 提交json
      * @return
      * @throws IOException
-     * @throws KeyManagementException
-     * @throws NoSuchAlgorithmException
-     * @throws ClientProtocolException
      */
-    public static <T> String sendJson(String url, T obj, String encoding) throws ClientProtocolException, IOException {
-        String body = "";
-
-        //tomcat是我自己的密钥库的密码，你可以替换成自己的
-        //如果密码为空，则用"nopassword"代替
-        SSLContext sslcontext = HttpClientUtils.getInstance().custom("D:\\keys\\wsriakey", "tomcat");
-
-        // 设置协议http和https对应的处理socket链接工厂的对象
-        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
-                .register("http", PlainConnectionSocketFactory.INSTANCE)
-                .register("https", new SSLConnectionSocketFactory(sslcontext))
-                .build();
-        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-        HttpClients.custom().setConnectionManager(connManager);
-
-        //创建自定义的httpclient对象
-        CloseableHttpClient client = HttpClients.custom().setConnectionManager(connManager).build();
-
-        //创建post方式请求对象
-        HttpPost httpPost = new HttpPost(url);
-
-        //装填参数
+    public <T> String postJson(String url, Map<String, String> queries, T obj) throws IOException {
+        String responseBody = "";
+        CloseableHttpClient httpClient = getHttpClient();
+        StringBuilder sb = new StringBuilder(url);
+        appendQueryParams(queries, sb);
+        //指定url,和http方式
+        HttpPost httpPost = new HttpPost(sb.toString());
+        if (SetTimeOut) {
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setSocketTimeout(SocketTimeout)
+                    .setConnectTimeout(ConnectTimeout).build();//设置请求和传输超时时间
+            httpPost.setConfig(requestConfig);
+        }
+        //添加参数
         String jsonbody = null;
         if (obj != null) {
             jsonbody = JSONObject.toJSONString(obj);
@@ -201,27 +246,50 @@ public class HttpClientUtils {
         stringEntity.setContentEncoding("UTF-8");
         stringEntity.setContentType("application/json");
         httpPost.setEntity(stringEntity);
-
-        System.out.println("请求地址：" + url);
-        System.out.println("请求参数：" + jsonbody);
-
-        //设置header信息
-        //指定报文头【Content-type】、【User-Agent】
-        httpPost.setHeader("Content-type", "application/json");
-        //httpPost.setHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
-
-        //执行请求操作，并拿到结果（同步阻塞）
-        CloseableHttpResponse response = client.execute(httpPost);
-        //获取结果实体
-        HttpEntity entity = response.getEntity();
-        if (entity != null) {
-            //按指定编码转换结果实体为String类型
-            body = EntityUtils.toString(entity, encoding);
-        }
-        EntityUtils.consume(entity);
-        //释放链接
-        response.close();
-        return body;
+        //请求数据
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        responseBody = getResponseString(responseBody, response);
+        return responseBody;
     }
 
+    /**
+     * 转换返回内容为string
+     * @param responseBody
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    private String getResponseString(String responseBody, CloseableHttpResponse response) throws IOException {
+        try {
+            logger.info(String.valueOf(response.getStatusLine()));
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                HttpEntity entity = response.getEntity();
+                responseBody = EntityUtils.toString(entity);
+            } else {
+                logger.info("http return status error:" + response.getStatusLine().getStatusCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            response.close();
+        }
+        return responseBody;
+    }
+
+    //Test
+    public static void main(String[] args) {
+        try {
+            String url = "https://bjjj.zhongchebaolian.com/common_api/mobile/standard/checksoft";
+            Android android = new Android();
+            android.setCurver("21");
+            android.setPlatform("android");
+            android.setAreacode("all");
+            android.setSoftkey("iiowner_android");
+            String response = HttpClientUtils.getInstance().postJson(url, null, android);
+            logger.debug(response);
+            JSONObject.parseObject(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
